094b176e8fb59b9b05b8205b516c4c520dcfcb4d
Fabricio Dourado <65264206+fvieir@users.noreply.github.com>
2021-07-14 18:11:22 -0300

    Primeiro commit

---
+++ database/migrations/2021_07_14_195256_create_telefones_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTelefonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telefones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('cliente_id');
+            $table->string('titulo');
+            $table->string('telefone');
+            $table->timestamps();
+        });
+
+        schema::table('telefones' ,function (Blueprint $table){
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telefones');
+    }
+}
