f7c8a626ba5ce83280f2fbf148b59e4c5ece67a4
Fabricio Dourado <65264206+fvieir@users.noreply.github.com>
2021-07-21 20:43:19 -0300

    Criação TelefoneController e metodos de adicão e edição

---
+++ app/Http/Controllers/TelefoneController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TelefoneController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function adicionar($id)
+    {
+        $cliente = \App\Cliente::find($id);
+        return view('telefone.adicionar' , \compact('cliente'));
+    }
+
+    public function salvar(Request $request, $id)
+    {
+        $telefone = new \App\Telefone;
+        $telefone->titulo = $request->input('titulo');
+        $telefone->telefone = $request->input('telefone');
+
+        \App\Cliente::find($id)->addTelefone($telefone);
+
+        \Session::flash('message', [
+            'msg' => "Telefone cadastrado com suscesso.",
+            'class' => "alert alert-success"
+        ]);
+
+        return \redirect()->route('cliente.detalhe' , $id);
+    }
+
+    public function editar($id)
+    {
+        $telefone = \App\Telefone::find($id);
+        return view('telefone.editar',\compact('telefone'));
+    }
+
+    public function atualizar(Request $request, $id)
+    {
+
+    }
+}
