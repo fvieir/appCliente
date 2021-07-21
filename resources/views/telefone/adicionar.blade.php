21445066ec1b2092d268706a3b3515ab4f0db5e3
Fabricio Dourado <65264206+fvieir@users.noreply.github.com>
2021-07-21 20:45:20 -0300

    criação da Telefone

---
+++ resources/views/telefone/adicionar.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-11">
+            <div class="card">
+                <div class="card-header">
+                    <ol>
+                        <a href="{{route('cliente.index')}}">Cliente </a>
+                        <a href="{{route('cliente.detalhe' , $cliente->id)}}"> / Detalhe </a>
+                        / Adicionar
+                    </ol>
+                </div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+            </div>
+
+            <div class="container">
+                @if(Session::has('messageEditar'))
+                <div class="{{ Session::get('messageEditar')['class']}}">
+                    {{Session::get('messageEditar')['msg']}}
+                </div>
+
+                @endif
+                <form action=" {{ route('telefone.salvar' , $cliente->id) }}" method = "post">
+                    {{ @csrf_field() }}
+    
+                    <div class="form-group">
+                        <label for="titulo">Titulo</label>
+                        <input type="text" class="form-control" name = "titulo" id="titulo" placeholder="Titulo do Telefone">
+                    </div>
+                    <div class="form-group">
+                        <label for="telefone">Telefone</label>
+                        <input type="text" class="form-control" name="telefone" id="telefone" placeholder="Nº do Telefone">
+                    </div>
+                    <button class="btn btn-info"> Adicionar </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
