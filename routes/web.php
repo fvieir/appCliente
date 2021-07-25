35d395f46c6d8e9015e1badb45d2b425335f6f5c
Fabricio Dourado <65264206+fvieir@users.noreply.github.com>
2021-07-25 11:10:51 -0300

    Adição de roda para exlcuir e editar telefone

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,11 @@ Route::get('/cliente/adicionar', ['uses' => 'ClienteController@adicionar', 'as' 
 Route::post('/cliente/salvar',['uses' => 'ClienteController@salvar','as' => 'cliente.salvar']);
 Route::get('/cliente/editar/{id}', ['uses' => 'ClienteController@editar', 'as' => 'cliente.editar']);
 Route::put('/cliente/atualizar/{id}',['uses' => 'ClienteController@atualizar','as' => 'cliente.atualizar']);
+Route::get('/cliente/deletar/{id}', ['uses' => 'ClienteController@deletar' , 'as' => 'cliente.deletar']);
+Route::get('/cliente/detalhe/{id}', ['uses' => 'ClienteController@detalhe', 'as' => 'cliente.detalhe']);
+
+Route::get('/telefone/adicionar/{id}' , ['uses' => 'TelefoneController@adicionar', 'as' => 'telefone.adicionar']);
+Route::post('/telefone/salvar/{id}',['uses' => 'TelefoneController@salvar', 'as' => 'telefone.salvar']);
+Route::get('/telefone/editar/{id}' , ['uses' => 'TelefoneController@editar', 'as' => 'telefone.editar']);
+Route::put('/telefone/atualizar/{id}' , ['uses' => 'TelefoneController@atualizar' , 'as' => 'telefone.atualizar']);
+Route::get('/telefone/deletar/{id}' , ['uses' => 'TelefoneController@deletar' , 'as' => 'telefone.deletar']);
